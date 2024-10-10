7a4980da25e74f1c8be2af2c3bba29fd08e488dc
j0hn1x <94726483+zksyncer@users.noreply.github.com>
2024-10-10 11:50:02 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,23 +1,3 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.alarms.create('fetchGasPrice', { periodInMinutes: 1/6 });
-});
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === 'fetchGasPrice') {
-    fetchGasPrice();
-  }
-});
-
-function updateIconBadge(price) {
-  const badgeText = price.toFixed(4);  // Display the price with 4 decimals on the badge
-  chrome.action.setBadgeText({ text: badgeText });
-  chrome.action.setBadgeBackgroundColor({ color: '#000000' });
-  chrome.action.setBadgeTextColor({ color: '#FFFFFF' });
-  
-  // Set a smaller font size for the badge text
-  chrome.action.setTitle({ title: `Scroll Gas Price: ${badgeText} Gwei` });
-}
-
 function fetchGasPrice() {
   fetch('https://rpc.scroll.io', {
     method: 'POST',
@@ -42,12 +22,9 @@ function fetchGasPrice() {
       const baseFeeWei = BigInt(data.result);
       const baseFeeGwei = Number(baseFeeWei) / 1e9; // Convert wei to Gwei
       chrome.storage.local.set({ gasPrices: [{ price: baseFeeGwei }] });
-      updateIconBadge(baseFeeGwei);
     })
     .catch(error => {
       console.error('Error fetching gas price:', error);
-      chrome.action.setBadgeText({ text: 'ERR' });
-      chrome.action.setBadgeBackgroundColor({ color: '#FF0000' });
     });
 }
 
