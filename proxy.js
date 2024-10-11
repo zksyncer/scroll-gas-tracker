3a431890eb026e36f4fac4f11d374f68f48ac591
j0hn1x <94726483+zksyncer@users.noreply.github.com>
2024-10-11 01:05:10 -0500

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,1 +1,22 @@
-node proxy.js
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.get('/gasprices', async (req, res) => {
+  try {
+    const response = await axios.get('https://scrollscan.com/gastracker');
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching gas prices');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy server running on http://localhost:${PORT}`);
+});
+
